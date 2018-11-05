005bb73f1b56a2f6acb1f17738aa4675e1164fbc
prakash bhatt <43993354+prakashbhatt1@users.noreply.github.com>
2018-11-05 22:21:07 +0530

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+#here we have only one app called todo 
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'todo',#so just go to your setting.py file and add app name 
+]
